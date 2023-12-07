a30b816339acfc3d319fa582f574ad0cb7b4cb71
Adithya sai <123888728+Adithyasai123@users.noreply.github.com>
2023-12-08 01:48:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')  # Fix: Use '.html' for the template file
+    return render_template('index.html')
 
-@app.route('/static/<path:filename>')  # Fix: Add closing bracket for the path parameter
+@app.route('/static/<path:filename>')
 def serve_static(filename):
     root_dir = os.path.dirname(os.path.abspath(__file__))
     return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9000)
+    app.run(host='0.0.0.0', port=9000, debug=False)
